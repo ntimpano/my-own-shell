d15de5bf5dbb9cf80620da55117624ef4a163f4d
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-12 14:30:23 -0300

    add return to shell

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ def main():
 
         # Si el comando está vacío, vuelve al inicio del bucle
         if not command:
+            sys.stdout.write("$ ")
             continue
 
         # Ejecutar el comando y verificar el código de salida
@@ -20,12 +21,15 @@ def main():
         # Si el comando falla (código de salida diferente de 0)
         if status.returncode != 0:
             sys.stdout.write(f"{command[0]}: command not found\n")
+            sys.stdout.write("$ ")
         else:
             # Mostrar la salida del comando si existe
             if status.stdout:
                 sys.stdout.write(status.stdout)
+                sys.stdout.write("$ ")
             if status.stderr:
                 sys.stdout.write(status.stderr)
+                sys.stdout.write("$ ")
         
         # Asegurar que la salida se imprima antes de volver al prompt
         sys.stdout.flush()
