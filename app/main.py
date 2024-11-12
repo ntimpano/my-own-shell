c0be9f86726a4e70460ba47b9c25388939fb5fa6
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-12 14:16:07 -0300

    add return to shell

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
         sys.stdout.flush()
 
         command = input()
-        status = subprocess.run(command, capture_output=True)
+        status = subprocess.run(command, capture_output=True, text=True)
         if status.returncode != 0:
             sys.stdout.write(f"{command}: command not found\n")
             sys.stdout.write("$ ")
