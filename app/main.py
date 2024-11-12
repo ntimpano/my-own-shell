2794a854e2b5bcb4c84fb1c3b7015a839e549003
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-12 14:19:05 -0300

    add return to shell

--- app/main.py
+++ app/main.py
@@ -1,21 +1,30 @@
 import sys
 import subprocess
 
-
 def main():
     while True:
-
+        # Mostrar el prompt
         sys.stdout.write("$ ")
         sys.stdout.flush()
 
-        command = input()
-        status = subprocess.run(command, capture_output=True, text=True)
-        if status.returncode != 0:
-            sys.stdout.write(f"{command}: command not found\n")
-            sys.stdout.write("$ ")
-        else:
-            sys.stdout.write(status.stdout)
+        # Leer y dividir el comando
+        command = input().split()
 
+        if not command:
+            continue  # Si no hay comando, vuelve a solicitar uno
+
+        # Ejecutar el comando
+        try:
+            status = subprocess.run(command, capture_output=True, text=True)
+            if status.returncode != 0:
+                # Comando fallido
+                sys.stdout.write(f"{command[0]}: command not found\n")
+            else:
+                # Mostrar la salida del comando
+                sys.stdout.write(status.stdout)
+        except FileNotFoundError:
+            sys.stdout.write(f"{command[0]}: command not found\n")
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
