4109eaf67bda1abf2a1f50750dfcaf001a92e2b8
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-12 13:17:47 -0300

    running shell script

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
 
     # Wait for user input
     command = input()
-    sys.stdout.write(f"{command}: command not found.\n")
+    sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
