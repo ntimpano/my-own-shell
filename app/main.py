af716695ea20f11f905f20997994960a24e0aeb5
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-12 14:50:13 -0300

    add exit 0

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
         sys.stdout.flush()
 
         # Read and split the command
-        command = input().split().strip()
+        command = input().strip().split()
 
         if not command:
             continue  # If no command, prompt again
