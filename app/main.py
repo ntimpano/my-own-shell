c5831a8ceef49cca9d6ae9953743a6ed1c69c765
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-13 12:30:50 -0300

    add type

--- app/main.py
+++ app/main.py
@@ -7,9 +7,15 @@ def main():
         command = input()
         if command == "exit 0":
             break
-        if command.startswith('echo'):
+        elif command.startswith('echo'):
             command = command.split()
             print(" ".join(command[1:]))
+        elif command.startswith('type'):
+            command = command.split()
+            if command[1] in ['echo', 'exit']:
+                print("".join(f'{command[1]} is a shell builtin'))
+            else:
+                print(f'{command[1]}: not found')
         else:
             print(f"{command}: command not found")
 if __name__ == "__main__":
