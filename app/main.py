df18965dad815ee5dbf0830f4a9f222c21b9b049
Nahuel Timpano <nahuel.timpano@ar.proveedor-mcd.com>
2024-11-13 11:37:10 -0300

    add exit 0

--- app/main.py
+++ app/main.py
@@ -1,38 +1,12 @@
 import sys
-import subprocess
-
 def main():
     while True:
-        # Show prompt
         sys.stdout.write("$ ")
         sys.stdout.flush()
-
-        # Read and split the command
-        command = input().strip().split()
-
-        if not command:
-            continue  # If no command, prompt again
-
-        # Execute the command
-        try:
-            status = subprocess.run(command, capture_output=True, text=True)
-            if command == 'exit 0':
-                sys.stdout.write(status.stdout)
-            if status.returncode != 0:
-                # Command failed
-                if status.stderr:
-                    sys.stdout.write(status.stderr)
-                else:
-                    sys.stdout.write(f"{command[0]}: command not found\n")
-            else:
-                # Show command output if it exists
-                if status.stdout:
-                    sys.stdout.write(status.stdout)
-        except FileNotFoundError:
-            sys.stdout.write(f"{command[0]}: command not found\n")
-
-        # Force flush to display output before returning to prompt
-        sys.stdout.flush()
-
+        # Wait for user input
+        command = input()
+        if command == "exit 0":
+            break
+        print(f"{command}: command not found")
 if __name__ == "__main__":
     main()
